ddd67b00e1238b5bb803f8955c86aa24df2c3416
Stephen Ruan <Stephen@Stephens-MacBook-Pro.local>
2017-11-08 18:04:27 -0800

    app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,3 +2,12 @@
         $(this).parent().parent().siblings(".btn:first-child").html($(this).text()+' <span class="caret"></span>');
         $(this).parent().parent().siblings(".btn:first-child").val($(this).text());
     });
+
+    $( document ).ready(function() {
+    $('.dropdown').each(function (key, dropdown) {
+        var $dropdown = $(dropdown);
+        $dropdown.find('.dropdown-menu a').on('click', function () {
+            $dropdown.find('button').text($(this).text()).append(' <span class="caret"></span>');
+        });
+    });
+});
